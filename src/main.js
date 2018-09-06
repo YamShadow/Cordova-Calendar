84e0b9d4f4b0570655481830a281e9be69f9e0c2
Mathieu YamiShadow <mathieu.nibas@gmail.com>
2018-09-06 16:52:00 +0200

    AJout du app

--- src/main.js
+++ src/main.js
@@ -11,3 +11,37 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
+var app = {  
+  // Application Constructor
+  initialize: function () {
+      this.bindEvents();        
+  },    
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function () {
+      document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function () {
+      app.receivedEvent('deviceready');
+      alert('loooooooool')
+      cordova.plugins.notification.local.schedule({
+        title: 'My first notification',
+        text: 'Thats pretty easy...',
+        foreground: true
+    });
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function (id) {
+      console.log('Received Event: ' + id);
+  }
+};
+
+app.initialize();  
